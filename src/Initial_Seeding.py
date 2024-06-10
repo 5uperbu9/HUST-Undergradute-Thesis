dff4de817a958d9287cabdf0843c5ef42e7f5e74
superbug <84830677+Superbbbbbb@users.noreply.github.com>
2024-06-11 00:52:21 +0800

    Update Initial_Seeding.py

--- src/Initial_Seeding.py
+++ src/Initial_Seeding.py
@@ -2,20 +2,29 @@ import numpy as np
 from sklearn.cluster import KMeans
 
 
-def initial_seeding(train_size, batch_size, X):
+def initial_seeding(data, train_size, batch_size, X_train):
+
+    if data > 2:
+        X = []
+        for i in range(train_size):
+            X.append(X_train[i].flatten())
+        X = np.array(X)
+    else:
+        X = X_train
 
     kmeans = KMeans(n_clusters=batch_size, init='k-means++')
     kmeans.fit(X)
     labels = kmeans.labels_
     centroids = kmeans.cluster_centers_
-    centers = []
 
+    centers = []
     for i in range(centroids.shape[0]):
+        # 簇中的点
         cluster_points = X[labels == i]
         cluster_points_idx = np.arange(train_size)[labels == i]
-        centroid = centroids[i]
-        distances = np.linalg.norm(cluster_points - centroid, axis=1)
-        closest_point_idx = np.argmin(distances)
-        centers.append(cluster_points_idx[closest_point_idx])
+
+        centroid = centroids[i]  # 质心
+        distances = np.linalg.norm(cluster_points - centroid, axis=1)  # 计算距离
+        centers.append(cluster_points_idx[np.argmin(distances)])  # 最近的点
 
     return centers
