c85fe905c45d2c4bbf30448c5555ac9dbf48574b
superbug <84830677+Superbbbbbb@users.noreply.github.com>
2024-06-11 00:50:55 +0800

    Update Classifiers.py

--- src/Classifiers.py
+++ src/Classifiers.py
@@ -18,39 +18,37 @@ class SoftmaxRegression:
         self.y_train = to_categorical(y_train)
         self.X_val = X_val
         self.y_val = y_val
-        self.val_size = X_val.shape[0]
         self.k = k
-        self.n = X_train.shape[1]
+        self.m = X_train.shape[1]
+        self.val_size = X_val.shape[0]
 
-    def calc(self, probs):
+    def acc_calc(self, probs):
+
         acc = 0.0
         for i in range(self.val_size):
             acc += np.argmax(probs[i]) == self.y_val[i]
-        acc /= self.val_size
-        return acc
+
+        return acc / self.val_size
 
     def fit(self, labelled, lr=0.01, tol=1e-5, epochs=50000):
 
-        labelled_size = len(labelled)
-        batch = self.X_train[labelled]
+        L_size = len(labelled)
+        X = self.X_train[labelled]
+        y = self.y_train[labelled].T
 
-        w = np.zeros([self.k, self.n])
-        cnt = 0
-        y_one_hot = self.y_train[labelled].T
+        w = np.zeros([self.k, self.m])
 
-        for i in range(epochs):
-            probs = softmax(w.dot(batch.T))
-
-            grad = (-lr / labelled_size) * (y_one_hot - probs).dot(batch)
-
+        for e in range(epochs):
+            probs = softmax(w.dot(X.T))
+            grad = (-lr / L_size) * (y - probs).dot(X)
+            w -= grad
             if (np.abs(grad) < tol).all():
                 break
-            cnt += 1
-            w -= grad
 
         probs = softmax(w.dot(self.X_val.T)).T
-        acc = self.calc(probs)
+        acc = self.acc_calc(probs)
         probs = softmax(w.dot(self.X_train.T)).T
+
         return probs, acc
 
 
@@ -62,11 +60,13 @@ class CNN:
         self.X_val = X_val
         self.y_val = to_categorical(y_val)
         self.k = k
+
         self.batch_size = batch_size
         self.img_size = img_size
         self.channel = channel
 
     def net_init(self):
+
         classifier = Sequential()
 
         classifier.add(Conv2D(32, (3, 3), input_shape=(self.img_size, self.img_size, self.channel), activation='relu'))
@@ -94,12 +94,17 @@ class CNN:
         return classifier
 
     def fit(self, labelled):
+
         classifier = self.net_init()
+
         early_stopping = EarlyStopping(monitor='loss', patience=5, restore_best_weights=True, verbose=0)
         classifier.fit(self.X_train[labelled], self.y_train[labelled], epochs=200, batch_size=self.batch_size,
                        callbacks=[early_stopping], verbose=0)
+
         probs = classifier.predict(self.X_train, verbose=0)
         acc = classifier.evaluate(self.X_val, self.y_val, verbose=0)[1]
+
         del classifier
         backend.clear_session()
+
         return probs, acc
